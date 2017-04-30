af90d95efc0e3ea81fc708700de3107a67739184
y.yoshida <yy.bochann.eg@gmail.com>
2017-04-30 19:34:24 +0900

    必要な画面のクラスとレイアウトの追加

---
+++ FremePhotoEditor/app/src/main/java/com/example/fremephotoeditor/activitys/FremeListActivity.java
@@ -0,0 +1,10 @@
+package com.example.fremephotoeditor.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 2017/04/30.
+ */
+
+public class FremeListActivity extends AppCompatActivity {
+}
